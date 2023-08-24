a6ff2ec53663a0728c6def2f3707c84985ba8cbb
Caio Centeio <cfcenteio@gmail.com>
2023-08-23 22:04:00 -0300

    rails yelp day 23

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[new create]
+  #   collection do
+  #     # Custom index
+  #     # /restaurants/whatever_you_decide
+  #     # verb :path
+  #     get :top #=> get '/restaurants/top', to: 'restaurants#top'
+  #   end
+
+  #   member do
+  #     # Custom show
+  #     # /restaurants/13/whatever_you_decide
+  #     # verb :path
+  #     get :chef #=> get '/restaurants/13/chef', to: 'restaurants#chef'
+  #   end
+  end
+
+  # resources :reviews, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom    = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: 555, category: 'chinese' }
+fire_place    = { name: "Fire Place", address: "Fire Realm", phone_number: 444, category: 'italian' }
+water_room    = { name: "Water Room", address: "Water Realm", phone_number: 333, category: 'japanese' }
+earth_palace    = { name: "Earth Palace", address: "Earth Realm", phone_number: 222, category: 'french' }
+air_bistro    = { name: "Air Bistro", address: "Air Realm", phone_number: 111, category: 'belgian' }
+
+[dishoom, fire_place, water_room, earth_palace, air_bistro].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
